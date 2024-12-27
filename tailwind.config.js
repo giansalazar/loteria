2400d9cbf4855f60348d4f71a6e8e06d107f5291
Giancarlo Salazar <giancarlosalazar97@gmail.com>
2024-12-27 16:53:56 -0600

    loteria2.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
-const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'great-vibes': ['"Great Vibes"', 'cursive'],
+        'dancing-script': ['"Dancing Script"', 'cursive'],
+      },
+    },
   },
   plugins: [
-    flowbite.plugin(),
-  ],
+    require('flowbite/plugin')
+  ]
 }
